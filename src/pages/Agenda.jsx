26301248befba4116d6a2fa3432c643ca9a59c64
Gu Dias <guudiaass@gmail.com>
2026-03-09 23:39:26 +0000

    File changes

--- src/pages/Agenda.jsx
+++ src/pages/Agenda.jsx
@@ -167,6 +167,7 @@ export default function Agenda() {
                 setSelectedAgendamento(null);
                 setShowForm(true);
               }}
+
               className="bg-gradient-to-r from-violet-600 to-purple-600 hover:from-violet-700 hover:to-purple-700"
             >
               <Plus className="w-4 h-4 mr-2" />
